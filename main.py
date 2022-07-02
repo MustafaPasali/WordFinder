f4acbd923817ad6562f236dd1c2945d952c5b33f
MstfPsl <rens370@gmail.com>
2022-07-03 01:04:42 +0300

    Find text from url method changed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from finder import word_finder
 
 word = "NoSQL"
 url = "https://aws.amazon.com/tr/nosql/"
-
-result = word_finder(word, get_text(url))
+text = get_text(url)
+print(text)
+result = word_finder(word, text)
 print(result)
--- url_text.py
+++ url_text.py
@@ -7,6 +7,10 @@ def get_text(url):
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
     r = requests.get(url, headers=header)
     soup = BeautifulSoup(r.content, "lxml")
-    text = soup.find('main').text
+    list = soup.find_all('p')
+
+    text = ""
+    for p in list:
+        text = text + p.text + '\n'
 
     return text
